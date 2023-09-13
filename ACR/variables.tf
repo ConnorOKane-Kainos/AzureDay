280ac8e4bb61b3b0ab6eb3cf624215f37bb5d0e5
Connor O'Kane <connor.o'kane@kainos.com>
2023-09-13 14:17:28 +0100

    updating files

---
+++ ACR/variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    type = string
+    default = "citilads"
+    description = "Name of the resource group"
+}
+
+variable "location" {
+  type = string
+  default = "uksouth"
+  description = "Name of the azure region"
+}
